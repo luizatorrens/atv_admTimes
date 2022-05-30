5f1eca29fcf60ec6db95294dda848e4e396572c0
luizatorrens <luiza.torrens.2006@gmail.com>
2022-05-30 15:13:18 -0300

    atividade dw

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>OI BRUNNNNNO &#x1F693;</header>
+  <main>OI VINI</main>
+  <footer>OI LARI</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /*padding: 2rem;*/
+  height: 100vh;
+
+  font-weight: normal;
+}
+header {
+  background-color: red;
+  color: rgb(219, 59, 219);
+  height: 33%;
+  font-size: 1.3rem;
+}
+footer {
+  background-color: blue;
+  color: rgb(219, 59, 219);
+  height: 33%;
+  font-size: 1.3rem;
+}
+main {
+  height: 33%;
+  background-color:white;
+  color: crimson;
+}
+</style>
