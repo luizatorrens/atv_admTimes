ee3b3c18e7690d5617608357c193edbf74ae4ce8
luizatorrens <luiza.torrens.2006@gmail.com>
2022-06-09 11:38:24 -0300

    continuação

--- src/views/JogadoresView.vue
+++ src/views/JogadoresView.vue
@@ -1,14 +1,55 @@
 <script>
+import { v4 as uuid } from "uuid";
 export default {
   data() {
     return {
-      jogadores: [
-        { id: 1, name: "Jogadoor 1" },
-        { id: 2, name: "Jogador 2" },
-        { id: 3, name: "Jogador 3" },
-        { id: 4, name: "Time 4" },
+      novo_time: "",
+      times: [
+        {
+          id: "f12bc7ce-5ca7-4cbf-ac6e-be3e59cb2862",
+          time: "time 1",
+        },
+        {
+          id: "b880659d-5d33-4607-ae19-8a22a738b509",
+          time: "time 2",
+        },
+        {
+          id: "0f97fea6-72ac-4ee0-bf1b-92516d3417e1",
+          time: "time 3",
+        },
+        {
+          id: "25c441be-e90a-4847-98a7-0d888e4c981f",
+          time: "time 4",
+        },
       ],
+      jogadores: [
+        {
+          id: "ac803529-5e2d-45b2-a907-ca993bb301ea",
+          nome: "jogador 1",
+        },
+        {
+          id: "610e1ad0-0302-492e-a686-9bfb62cc2a82",
+          nome: "jogador 2",
+        },
+        {
+          id: "29eaba8d-a376-485e-9509-5dddd6587f0d",
+          nome: "jogador 3",
+        },
+        {
+          id: "d7c068f0-928f-4858-9ae3-eadc32edb230",
+          nome: "jogador 4",
+        },
+      ]
     };
+  },
+  methods: {
+    salvar() {
+      const novo_id = uuid();
+      this.times.push({
+        id: novo_id,
+        name: this.novo_time,
+      });
+    },
   },
 };
 </script>
@@ -19,8 +60,9 @@ export default {
       <h2>Gerencimento de Jogadores</h2>
     </div>
     <div class="form-input">
-      <input type="text" />
-      <button>Salvar</button>
+      <input type="text" v_model="novo_nome" placeholder="Nomes" />
+      <input type="text" v_model="novo_time" placeholder="Times" />
+      <button @click="salvar">Salvar</button>
     </div>
     <div class="list-items">
       <table>
@@ -28,7 +70,7 @@ export default {
           <tr>
             <td>ID</td>
             <td>Nomes</td>
-            <td>Time ID</td>
+            <td>Time</td>
             <td>Ações</td>
           </tr>
         </thead>
@@ -36,8 +78,8 @@ export default {
           <tr v-for="jogador in jogadores" :key="jogador.id">
             <td>{{ jogador.id }}</td>
             <td>{{ jogador.name }}</td>
+            <td>{{ jogador.time }}</td>
             <td>???</td>
-            <td>Time 1</td>
           </tr>
         </tbody>
       </table>
--- src/views/TimesView.vue
+++ src/views/TimesView.vue
@@ -1,14 +1,25 @@
 <script>
+import { v4 as uuid } from "uuid";
 export default {
   data() {
     return {
+      novo_time: "",
       times: [
-        { id: 1, name: "Time 1" },
-        { id: 2, name: "Time 2" },
-        { id: 3, name: "Time 3" },
-        { id: 4, name: "Time 4" },
+        { id: "f12bc7ce-5ca7-4cbf-ac6e-be3e59cb2862", name: "Time 1" },
+        { id: "b880659d-5d33-4607-ae19-8a22a738b509", name: "Time 2" },
+        { id: "0f97fea6-72ac-4ee0-bf1b-92516d3417e1", name: "Time 3" },
+        { id: "25c441be-e90a-4847-98a7-0d888e4c981f", name: "Time 4" },
       ],
     };
+  },
+  methods: {
+    salvar() {
+      const novo_id = uuid();
+      this.times.push({
+        id: novo_id,
+        name: this.novo_time,
+      });
+    },
   },
 };
 </script>
@@ -19,9 +30,8 @@ export default {
       <h2>Gerencimento de Times</h2>
     </div>
     <div class="form-input">
-      <input type="text" placeholder="Nomes" />
-      <input type="text" placeholder="Times" />
-      <button>Salvar</button>
+      <input type="text" v-model="novo_time" placeholder="Times" />
+      <button @click="salvar">Salvar</button>
     </div>
     <div class="list-items">
       <table>
